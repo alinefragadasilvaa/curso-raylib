baac37089708ff6b7210517f88cf321f4eb2551d
Aline Fraga da Silva <109742288+alinefragadasilvaa@users.noreply.github.com>
2023-08-02 11:36:29 -0300

    Update aula3.c

--- aula3.c
+++ aula3.c
@@ -1,60 +1,61 @@
 #include "raylib.h"
 
-// tipos pré definidos na biblioteca
-
-// vector2 V
-// V.x = x;
-// V.y = y;
-
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    // é preciso inicializar a janela antes de usar as funções GetMouse
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    Rectangle start = (Rectangle) {x: (screenWidth-MeasureText("Start", 30))/2,
+    // variáveis de tipos pré-definidos
+    Rectangle start = (Rectangle) {x: (screenWidth-MeasureText("Start", 30))/2, // tipo retângulo
                                    y: (screenHeight-30)/2,
                                    width: MeasureText("Start", 30),
-                                   height: 30};
-
-    Vector2 mouse;
-    Color cor_palavra = RED, cor_fundo = BLACK, cor_mudando =  (Color) {r: 127, g:50, b: 255, a:255};
+                                   height: 30}; // os valores desse retângulo serão usados para detectar colisão do mouse em uma string de mesmos valores
+    
+    Vector2 mouse; // tipo vetor tipo 2
+    
+    Color cor_palavra = RED, cor_fundo = BLACK; // tipo cor
+    Color cor_mudando =  (Color) {r: 127, g:50, b: 255, a:255}; // as cores podem ser criadas mudando a intensidade das cores primárias
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()){
 
-        mouse = (Vector2) {GetMouseX(), GetMouseY()}; // tem que ser dentro do while para atualizar o valor
+        // ÁREA DE PROGRAMAÇÃO
 
-        cor_mudando.r+=5;
+        mouse = (Vector2) {GetMouseX(), GetMouseY()}; // atualizando o vetor da posição do mouse a cada iteração
+
+        // cor_mudando.r+=5; para mudar a intensidade da cor a cada iteração
+
+        // ÁREA DE DESENHO
 
         BeginDrawing();
 
+        // mudando a cor do fundo usando entrada do mouse
         if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)){
             cor_fundo = WHITE;
         } else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
             cor_fundo = BLACK;
         }
 
-        ClearBackground(cor_fundo);
+        ClearBackground(cor_fundo); // cor do fundo variável
 
+        // colisão do mouse com a posição da matriz
         if(CheckCollisionPointRec(mouse, start)){
-            cor_palavra = GREEN;
+            cor_palavra = GREEN; // cor com mouse
         } else {
-            cor_palavra = RED;
+            cor_palavra = RED; // cor sem mouse
         }
 
-        // DrawRectangleRec(start, WHITE);
+        // DrawRectangleRec(start, WHITE); para testar a existÊncia do retângulo abaixo da string
 
-        DrawText("Start", start.x, start.y, start.height, cor_palavra);
+        DrawText("Start", start.x, start.y, start.height, cor_palavra); // string com os mesmos valores do retângulo
 
         EndDrawing();
 
     }
-
 
     CloseWindow();
 
