d6236096a769d194fafade5fc16c1fcb5ce359cd
Aline Fraga da Silva <109742288+alinefragadasilvaa@users.noreply.github.com>
2023-08-02 11:06:22 -0300

    Update desafioDVD-versao2.c

--- desafioDVD-versao2.c
+++ desafioDVD-versao2.c
@@ -1,87 +1,46 @@
- /*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    // minhas variáveis
-    int i=0, j=0;
-    int dx=2, vx=2;
-    char C[4] = {"DVD"};
+    int x=0, y=0; // cordenadas da string
+    int dx=2, dy=2; // direções da string nas cordenadas
+    char S[4] = {"DVD"}; // string
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    SetTargetFPS(60);             
+ 
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+      
+        // ÁREA DE PROGRAMAÇÃO
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        // icrementando as cordenadas com as direções
+        x+=dx; 
+        y+=dy;
 
-        // a ideia é fazer a string "DVD" percorrer pela janela sem ultrapassar os limites da mesma
+        // ÁREA DE DESENHO
 
         BeginDrawing();
 
-            i+=dx; // string percorrendo a janela linearmente incrementando a coordenada i
-            j+=vx;
+        ClearBackground(BLACK);
 
-            ClearBackground(BLACK);
-
-
-
-            if(i>screenWidth-MeasureText(C,100) || i<0){
-               dx*=-1;
-            } else if(j>screenHeight-100 || j<0){
-               vx*=-1;
-            } else{
-                DrawText(C, i, j, 100, GREEN);
-            }
+        if(x>screenWidth-MeasureText(S,100) || x<0){
+           dx*=-1;
+        } else if(y>screenHeight-100 || y<0){
+           dy*=-1;
+        } else{
+            DrawText(S, x, y, 100, GREEN);
+        }
 
         EndDrawing();
 
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow(); 
 
     return 0;
 }
