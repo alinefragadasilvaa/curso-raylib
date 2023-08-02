c6209eb36a9dc56152f49b082fd6791a5362b34e
Aline Fraga da Silva <109742288+alinefragadasilvaa@users.noreply.github.com>
2023-08-02 10:40:17 -0300

    Update aula1.c

--- aula1.c
+++ aula1.c
@@ -1,76 +1,40 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+/*NESTA AULA FORAM INTRODUZIDOS CONCEITOS DE FUNCIONAMENTO DA JANELA E DE CORDENADAS, USANDO FUNÇÃO
+QUE DESENHA TEXTOS*/
 
 #include "raylib.h" // incluindo a biblioteca
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    // largura e altura da janela
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    // minhas variáveis
-    int i;
+    // variáveis usadas na janela
+    int i; // contador das iterações
     char C[4][50] = {"oi", "tchau", "a", "gremio"}; // strings para desenhar na janela
 
-    InitWindow(screenWidth, screenHeight, "Aula 1"); // inicializando a janela
+    // inicialização da janela
+    InitWindow(screenWidth, screenHeight, "Aula 1"); 
 
-    SetTargetFPS(60);               // quantidade de vezes que a janela inicializa por segundo (a janela é um while)
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key 
+    SetTargetFPS(60);  // frames por segundo
+    
+    while (!WindowShouldClose())  // função indica o fechamento da janela ou ESC
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+        // ÁREA DE DESENHO
 
         BeginDrawing();
 
-            ClearBackground(BLACK); // cor do fundo
+        ClearBackground(BLACK); // cor do fundo da janela
 
-            // desenhando cada string em uma coordenada diferente
-            for(i=0;i<4;i++){
-                DrawText(C[i], (screenWidth-MeasureText(C[i],30))/2,100+(30+50)*i, 30, RED); // MeasureText() devolve a largura da string
-            }
+        // desenhando cada string em uma coordenada diferente
+        for(i=0;i<4;i++){
+            DrawText(C[i], (screenWidth-MeasureText(C[i],30))/2,100+(30+50)*i, 30, RED); // MeasureText() devolve a largura da string
+        }
 
         EndDrawing();
-
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow(); 
+    
     return 0;
 }
