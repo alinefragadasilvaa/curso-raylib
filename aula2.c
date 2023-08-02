113d0d2edc10464549f21ebba5ad2bad32f5b421
Aline Fraga da Silva <109742288+alinefragadasilvaa@users.noreply.github.com>
2023-08-02 10:19:49 -0300

    Update aula2.c

--- aula2.c
+++ aula2.c
@@ -1,89 +1,50 @@
- /*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+/*NESTA AULA, A JANELA FOI ESTRUTURADA COMO UMA MATRIZ, FORAM UTILIZADAS
+FUNÇÕES DE ENTRADAS DO MOUSE E DE POSIÇÃO DO MOUSE PARA ALTERAR OS VALORES 
+DAS POSIÇÕES DAS MATRIZ E, COM BASE NISSO, DESENHAR RETÂNGULOS DIFERENTES NA JANELA*/
 
-#include "raylib.h"
+// inclusão da biblioteca
+#include "raylib.h" 
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 500;
-    const int screenHeight = 500;
+    // largura e altura da janela
+    const int screenWidth = 500; 
+    const int screenHeight = 500; 
 
-    // minhas variáveis
-    int i, j;
-    int sb=50;
-    int tabela[10][10]={0};
+    // variáveis usadas para resolver os desafios
+    int i, j; // iterações
+    int sb=50; // sizebox
+    int tabela[10][10]={0}; // matriz do mapa
 
+    // inicialização da janela
+    InitWindow(screenWidth, screenHeight, "AULA 3");
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    // quantidade de frames por segundo
+    SetTargetFPS(60);            
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    // Mloop da janela
+    while (!WindowShouldClose()) // finaliza com o fechamento da janela ou ESC
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        // ÁREA DE PROGRAMAÇÃO
 
-        // Draw
-        //----------------------------------------------------------------------------------
-
-        // a ideia é fazer a string "DVD" percorrer pela janela sem ultrapassar os limites da mesma
-
+        // uso de funções que detectam entradas pelo mouse e constantes que representam os botões
         if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
-           tabela[GetMouseX()/sb][GetMouseY()/sb] =1;
+           tabela[GetMouseX()/sb][GetMouseY()/sb] =1; // GetMouseX e GetMouseY retornam as cordenadas x e y do mouse
         } else if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
-           tabela[GetMouseX()/sb][GetMouseY()/sb] =2;
+           tabela[GetMouseX()/sb][GetMouseY()/sb] =2; // as cordenadas divididas pelo tamanho do quadro retornam um valor inteiro que corresponde a posição na matriz do mapa
         } else if(IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)){
            for(i=0;i<10;i++){
                 for(j=0;j<10;j++){
-                    tabela[i][j]=0;
+                    tabela[i][j]=0; // os valores na posição da matriz são usados para desenhar quadros deiferentes
                 }
             }
         }
 
+        // ÁREA DE DESENHO
+     
         BeginDrawing();
 
-            /*inicio+=dx; // string percorrendo a janela linearmente incrementando a coordenada i
-            j+=vx;
-
-
-            if(inicio>screenWidth-MeasureText(C,100) || inicio<0){
-               dx*=-1;
-            } else if(j>screenHeight-100 || j<0){
-                vx*=-1;
-            } else {
-                DrawText(C, inicio, j, 100, GREEN);
-            }*/
-
-            ClearBackground(BLACK);
+        ClearBackground(BLACK); // cor de fundo da janela
 
             /*for(i=0;i<10;i++){
                 for(j=0;j<10;j++){
@@ -95,10 +56,10 @@ int main(void)
                     }
                 }
             }*/
-
+     
+            // desenhando matriz do mapa
             for(i=0;i<10;i++){
                 for(j=0;j<10;j++){
-
                     switch(tabela[i][j]){
                         case 1:
                             DrawRectangle(i*sb,j*sb,sb,sb,BLUE);
@@ -110,7 +71,7 @@ int main(void)
                             DrawRectangle(i*sb,j*sb,sb,sb,BLACK);
                         break;
                     }
-                     DrawRectangleLines(i*sb,j*sb,sb,sb,WHITE);
+                    DrawRectangleLines(i*sb,j*sb,sb,sb,WHITE); // função que desenha apenas as linhas do quadrado
                 }
             }
 
@@ -118,14 +79,9 @@ int main(void)
             //(GetMouseX(),GetMouseY(),sb,sb,GREEN);
 
         EndDrawing();
-
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+ 
+    CloseWindow();       
 
     return 0;
 }
